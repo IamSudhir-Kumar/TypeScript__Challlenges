8aba645a357b92330ec2f3fecf68487a53743ad8
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-10-07 22:53:16 +0530

    -----

---
+++ 1.ts
@@ -0,0 +1,8 @@
+// Introduction to TypeScript
+// Setting up a TypeScript project
+// Variables and data types in TypeScript
+// Comparison operators and logical operators in TypeScript
+// Conditionals (if, else if, else) in TypeScript
+
+const character:number = 4;
+console.log(character);
