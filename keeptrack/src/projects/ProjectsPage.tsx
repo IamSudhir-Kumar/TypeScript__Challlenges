dffebe33c75abf52c7137cae11cdd90572947797
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-11-03 19:55:19 +0530

    Lab-7 displaying Data

--- keeptrack/src/projects/ProjectsPage.tsx
+++ keeptrack/src/projects/ProjectsPage.tsx
@@ -1,7 +1,14 @@
+import { MOCK_PROJECTS } from "./MockupProjects";
+import { Project } from "./Project";
+import ProjectList from "./ProjectList";
+
 function ProjectsPage() {
   return (
-    <h1>ProjectsPage</h1>
-  )
+    <>
+      <h1>ProjectsPage</h1>
+      <ProjectList projects={MOCK_PROJECTS} />
+    </>
+  );
 }
 
-export default ProjectsPage
+export default ProjectsPage;
